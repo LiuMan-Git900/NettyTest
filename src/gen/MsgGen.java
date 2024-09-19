133f8a053741a0f0b91d3a1c4d07c2fc6d81c908
NettyTest <2215662907@qq.com>
2024-09-19 16:27:42 +0800

    @mod Freemark的源代码

--- src/gen/MsgGen.java
+++ src/gen/MsgGen.java
@@ -1,10 +1,13 @@
 package gen;
 
+
+import freemarker.core.Configurable;
 import freemarker.template.Configuration;
 
 // 我们使用的是模板生成
 public class MsgGen {
     // 普通的文件生成
     // freemarkker模板生成
-
+    Configuration cfg = new Configuration(Configuration.VERSION_2_3_22);
+    cfg.setCacheStorage();
 }
